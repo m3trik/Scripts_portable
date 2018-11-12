c08b547e5319effe819e545a01dcb37c63584a2b
m3trik <m3trik@live.com>
2018-11-12 10:36:31 -0500

    Initial commit

---
+++ test-001.py
@@ -0,0 +1,10 @@
+import pymxs, MaxPlus as mp
+rt = pymxs.runtime
+
+
+#rt.selectByName(filter=lambda x: rt.isKindOf(x, rt.Box))
+
+nodes = mp.SelectionManager.Nodes
+
+dir = mp.FileManager_ChooseDirectory
+
