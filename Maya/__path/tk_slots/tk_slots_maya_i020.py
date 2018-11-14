ed01d8f03dd5bd897cdb156be02e35c4570a56e0
m3trik <m3trik@live.com>
2018-11-14 13:58:44 -0500

    -

---
+++ Maya/__path/tk_slots/tk_slots_maya_i020.py
@@ -0,0 +1,55 @@
+import maya.mel as mel
+import pymel.core as pm
+
+import os.path
+
+from tk_slots_max_init import Init
+
+
+
+
+
+class I020(Init):
+	def __init__(self, *args, **kwargs):
+		super(I020, self).__init__(*args, **kwargs)
+
+		#init widgets
+		self.initWidgets(self)
+
+
+	def b000(self): #
+		pass
+
+	def b001(self): #
+		mel.eval('')
+
+	def b002(self): #
+		mel.eval('')
+
+	def b003(self): #
+		mel.eval('')
+
+	def b004(self): #
+		mel.eval('')
+
+	def b005(self): #
+		mel.eval('')
+
+	def b006(self): #
+		mel.eval('')
+
+	def b007(self): #
+		mel.eval('')
+
+	def b008(self): #
+		mel.eval("")
+
+	def b009(self): #
+		mel.eval('')
+
+
+#print module name
+print os.path.splitext(os.path.basename(__file__))[0]
+# -----------------------------------------------
+# Notes
+# -----------------------------------------------
