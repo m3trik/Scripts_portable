2b87c897c39169fefc56f53900edbc413ef259f0
m3trik <m3trik@live.com>
2018-12-05 22:00:37 -0500

    -

--- max/userSetup.py
+++ max/userSetup.py
@@ -20,8 +20,8 @@ import sys, os.path
 
 
 #Append directory to system path -----------------
-paths = r'%CLOUD%/____Graphics/__general/_portable/_scripts/__path;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/tk_ui;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/max;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/max/tk_slots_max'
-# paths = r'%USERPROFILE%/Documents/_portable/_scripts/__path;%USERPROFILE%/Documents/_portable/_scripts/__path/tk_ui;%USERPROFILE%/Documents/_portable/_scripts/__path/max;%USERPROFILE%/Documents/_portable/_scripts/__path/max/tk_slots_max'
+# paths = r'%CLOUD%/____Graphics/__general/_portable/_scripts/__path;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/tk_ui;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/max;%CLOUD%/____Graphics/__general/_portable/_scripts/__path/max/tk_slots_max'
+paths = r'%USERPROFILE%/Documents/_portable/_scripts/__path;%USERPROFILE%/Documents/_portable/_scripts/__path/tk_ui;%USERPROFILE%/Documents/_portable/_scripts/__path/max;%USERPROFILE%/Documents/_portable/_scripts/__path/max/tk_slots_max'
 for path in paths.split(';'):
 	sys.path.append(os.path.expandvars(path))
 
