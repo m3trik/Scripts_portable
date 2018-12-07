abb9103bf368cd943a6fbcb2e0fc95009fd0b7cc
m3trik <m3trik@live.com>
2018-12-07 09:30:29 -0500

    -

--- max/_test_004.py
+++ max/_test_004.py
@@ -2,6 +2,14 @@ import MaxPlus; maxEval = MaxPlus.Core.EvalMAXScript
 from pymxs import runtime as rt
 
 
+#~ sel = rt.getCurrentSelection()
+#~ print sel
+
+#~ source = sel[0]
+#~ target = sel[1]
+
+#~ source.center = target.center
+
 sel = [s for s in rt.getCurrentSelection()]
 
 objects = sel[:-1]
---
+++ max/_test_005.py
@@ -0,0 +1,9 @@
+import MaxPlus; maxEval = MaxPlus.Core.EvalMAXScript
+from pymxs import runtime as rt
+
+
+
+
+
+#~ copy. if not valid object uncheck.
+#~ lock xyz
